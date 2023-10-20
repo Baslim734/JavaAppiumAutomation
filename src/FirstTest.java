82c0833c9f5da8d5de711726cca4d89193d5c397
Baslim734 <nikita7343072@gmail.com>
2023-10-20 17:42:11 +0300

    Третий урок задание Ex 2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -35,8 +40,64 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
-    {
-        System.out.println("First test run");
+    public void testCompareInstalledTextInField(){
+        //так как android.widget.TextView не содержит своего уникального id,а завязаться на класс не самая лучшая идея
+        // (так как android.widget.TextView может быть несколько на странице)
+        assertElementHasText(By.xpath("//*[contains(@text,'Search Wikipedia')]"),"Search Wikipedia",
+                "'Search Wikipedia' was not found in text field"
+                );
+
     }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+            ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message){
+       return waitForElementPresent(by,error_message,5);
+    }
+    private WebElement waitForElementAndClick(By by, String error_message){
+        WebElement element = waitForElementPresent(by,error_message,5);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by,String value, String error_message){
+        WebElement element = waitForElementPresent(by,error_message,5);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message,long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private  WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    private WebElement assertElementHasText(By by,String textInElement, String error_message){
+        WebElement tittle_element = waitForElementPresent(
+               by, error_message
+        );
+        String article_tittle = tittle_element.getAttribute("text");
+
+        Assert.assertEquals(
+                error_message,
+                textInElement,
+                article_tittle
+        );
+        return tittle_element;
+    }
+
 }

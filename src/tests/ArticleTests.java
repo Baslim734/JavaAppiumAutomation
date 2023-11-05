6416b9ae4a09b3db34d7f59d06eaa7423c1fe8c2
Baslim734 <nikita7343072@gmail.com>
2023-11-05 15:29:29 +0300

    Ex 8 Первое задание пятого урока

--- src/tests/ArticleTests.java
+++ src/tests/ArticleTests.java
@@ -1,8 +1,13 @@
+package tests;
+
 import lib.CoreTestCase;
-import lib.ui.*;
+import lib.ui.ArticlePageObject;
+import lib.ui.MyListsPageObject;
+import lib.ui.NavigationUI;
+import lib.ui.SearchPageObject;
 import org.junit.Test;
 
-public class FirstTest extends CoreTestCase {
+public class ArticleTests extends CoreTestCase {
 
     private SearchPageObject SearchPageObject;
     private ArticlePageObject ArticlePageObject;
@@ -12,52 +17,6 @@ public class FirstTest extends CoreTestCase {
     protected void setUp() throws Exception {
         super.setUp();
         SearchPageObject = new SearchPageObject(driver);
-    }
-
-    @Test
-    public void testSearch() {
-        SearchPageObject = new SearchPageObject(driver);
-
-        SearchPageObject.startAppSkipButton();
-        SearchPageObject.initSearchInput();
-        SearchPageObject.typeSearchLine("Java");
-        SearchPageObject.waitForSearchResult("Object-oriented programming language");
-    }
-
-    @Test
-    public void testSearchAndCancel() {
-        SearchPageObject = new SearchPageObject(driver);
-
-        SearchPageObject.startAppSkipButton();
-        SearchPageObject.initSearchInput();
-        SearchPageObject.typeSearchLine("Oil");
-        SearchPageObject.waitForSearchResult("Viscous water-insoluble liquid");
-        SearchPageObject.waitForSearchResult("Programme headed by the United Nations");
-        SearchPageObject.waitForSearchResult("Oil platform");
-        SearchPageObject.closeSearch();
-        SearchPageObject.waitForSearchResultDissapear("Viscous water-insoluble liquid");
-        SearchPageObject.waitForSearchResultDissapear("Programme headed by the United Nations");
-        SearchPageObject.waitForSearchResultDissapear("Oil platform");
-    }
-
-    @Test
-    public void testCompareInstalledTextInField() {
-        SearchPageObject = new SearchPageObject(driver);
-
-        SearchPageObject.startAppSkipButton();
-        SearchPageObject.compareSearchInputedText("Search Wikipedia");
-
-    }
-
-    @Test
-    public void testCompareWordsInSearchResults() {
-        SearchPageObject = new SearchPageObject(driver);
-        ArticlePageObject = new ArticlePageObject(driver);
-
-        SearchPageObject.startAppSkipButton();
-        SearchPageObject.initSearchInput();
-        SearchPageObject.typeSearchLine("Oil");
-        ArticlePageObject.expectTheTextInEachResult("Oil");
     }
 
     @Test
---
+++ src/tests/SearchTests.java
@@ -0,0 +1,64 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class SearchTests extends CoreTestCase {
+
+    private SearchPageObject SearchPageObject;
+    private lib.ui.ArticlePageObject ArticlePageObject;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        SearchPageObject = new SearchPageObject(driver);
+    }
+
+    @Test
+    public void testSearch() {
+        SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.startAppSkipButton();
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForSearchResult("Object-oriented programming language");
+    }
+
+    @Test
+    public void testSearchAndCancel() {
+        SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.startAppSkipButton();
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Oil");
+        SearchPageObject.waitForSearchResult("Viscous water-insoluble liquid");
+        SearchPageObject.waitForSearchResult("Programme headed by the United Nations");
+        SearchPageObject.waitForSearchResult("Oil platform");
+        SearchPageObject.closeSearch();
+        SearchPageObject.waitForSearchResultDissapear("Viscous water-insoluble liquid");
+        SearchPageObject.waitForSearchResultDissapear("Programme headed by the United Nations");
+        SearchPageObject.waitForSearchResultDissapear("Oil platform");
+    }
+
+    @Test
+    public void testCompareInstalledTextInField() {
+        SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.startAppSkipButton();
+        SearchPageObject.compareSearchInputedText("Search Wikipedia");
+
+    }
+
+    @Test
+    public void testCompareWordsInSearchResults() {
+        SearchPageObject = new SearchPageObject(driver);
+        ArticlePageObject = new ArticlePageObject(driver);
+
+        SearchPageObject.startAppSkipButton();
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Oil");
+        ArticlePageObject.expectTheTextInEachResult("Oil");
+    }
+
+}
